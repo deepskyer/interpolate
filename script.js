73da079ee0b91a367563e0a3941b72742ce88d40
deepskyer <44412623+deepskyer@users.noreply.github.com>
2020-01-23 12:00:55 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-//window.onload = () => console.log("running");
-
 window.onload = () => {
   var marker = ["{{", "}}"];
 
+  //如果元素里面的文字有marker标注
   var match = function(item) {
     return (
       item.textContent.indexOf(marker[0]) > -1 &&
@@ -11,25 +10,30 @@ window.onload = () => {
     );
   };
   var interpolate = function(props) {
-    var NodeList = document.querySelectorAll("[label]"); //定位所有标注的元素
-    var elements = Array.from(NodeList); //把Nodelist转成array
-    console.log(elements);
+    //定位所有标注的元素, <div label>{{name }}</div>
+    var NodeList = document.querySelectorAll("[label]");
+    //把Nodelist转成array
+    var elements = Array.from(NodeList);
 
     elements.forEach(function(element) {
       if (match(element) === true) {
-        var val = element.textContent.slice(
+        //取出marker的内容, 去掉空格
+        val = element.textContent.slice(
           2, //因为有两个{{
           element.textContent.lastIndexOf(marker[1])
-        ); //得到要传数据的key
-        //找到对应的值
-        console.log(val);
+        ).split(' ').join('');
+        //通过传入的参数（props Object) 找到对应的值进行替换
+        if (props.hasOwnProperty(val) && val != null){
+          element.textContent = props[val];
+        }
 
-        console.log(props[0].val);
       }
     });
   };
+
+  // call the fucntion, 传入替换值
   interpolate({
-    name: "james",
+    name: "Steven",
     age: 18
   });
 };
